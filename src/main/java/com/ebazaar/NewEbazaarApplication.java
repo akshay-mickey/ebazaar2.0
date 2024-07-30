fe006a8ccda2c8ce4f5c128299839ae1f5f99a70
akshay <akshay@gmail.com>
2024-07-30 07:40:34 +0000

    inital project 2.1 setup

---
+++ src/main/java/com/ebazaar/NewEbazaarApplication.java
@@ -0,0 +1,13 @@
+package com.ebazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewEbazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewEbazaarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshay/Football/NewEbazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.Football;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewEbazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
